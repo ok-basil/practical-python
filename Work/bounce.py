826b400263f802bb0454fd56655e55bfbee5fc6e
Basil <basilokache@gmail.com>
2021-01-28 09:35:05 +0100

    Updated work

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,15 @@
 # bounce.py
 #
 # Exercise 1.5
+# Define formula and function
+original_height = 100 #meters
+bounce_reduction = 0.6
+new_height = original_height*bounce_reduction
+bounce=1
+while bounce <=10
+    original_height*bounce_reduction <= 10:
+    print(bounce, new_height)
+    bounce=bounce + 1
+    original_height = new_height
+
+print(bounce, new_height)
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+def purchase(filename):
+    infile = open(filename, 'rt')
+    headers = next(infile).split(',')
+    total_cost = 0
+    for line in infile:
+        row = line.split(',')
+        shares= int(row[1])
+        prices = float(row[2])
+        total_cost += shares*prices
+    print(total_cost)
